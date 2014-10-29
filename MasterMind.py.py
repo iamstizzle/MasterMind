cf1c401a1dabe935a406036d34bb06d4bb00fd19
iamstizzle <thatguyjoshua@yahoo.com>
2014-10-29 10:31:03 -0500

    Tweaks to interface
    Prints still look like garbage, but I am storing each guess paired with
    the results of the wincheck. May need to hide those and instead print
    out a list of the results per user prompt.

--- MasterMind.py.py
+++ MasterMind.py.py
@@ -1,10 +1,10 @@
-
-################################### SETUP ###########################################################
+#---------------------------------------
+############# SETUP ####################
+#---------------------------------------
 
 from random import randint
 import time
 import random
-
 
 def d6():
     return randint(1,6)
@@ -22,7 +22,8 @@ guessoptions = {
     5 : "White",
     6 : "Yellow"
     }
-########
+
+#####----Class ---- #######
 
 class codecreator(object):
     def __init__(self):
@@ -31,6 +32,7 @@ class codecreator(object):
         self.placeholder = []
         self.guess = []
         self.answer = []
+        self.colorpeg = []
         self.answercount = {
             "Red" : 0,
             "Green" : 0,
@@ -48,9 +50,7 @@ class codecreator(object):
             "Yellow" : 0
             }
         
-    def random_code(self):
-        self.answer = [ guessoptions[d6()],guessoptions[d6()],guessoptions[d6()],guessoptions[d6()] ]
-        
+  
     def codereset(self):
         self.answer = []
         self.guess = []
@@ -70,6 +70,10 @@ class codecreator(object):
             "White" : 0,
             "Yellow" : 0
             }
+
+
+    def random_code(self):
+        self.answer = [ guessoptions[d6()],guessoptions[d6()],guessoptions[d6()],guessoptions[d6()] ]
         
     def custom_code(self):
         self.codereset()
@@ -89,9 +93,29 @@ class codecreator(object):
                     validinput = False
         self.answer = [ guessoptions[customguess[0]], guessoptions[customguess[1]], guessoptions[customguess[2]], guessoptions[customguess[3]] ]
 
+
+
+    def guessing(self):
+        answerguess = []
+        answerfinal = []
+        userinput = []
+        while len(answerguess) < 4:
+            validinput = True
+            while validinput == True and len(answerguess)<4:
+                userinput =raw_input("Your Guess options are:  1= Red, 2 = Green, 3= Blue, 4 = Black, 5 = White, 6 = Yellow :")
+                ## use raw_input instead of input() http://stackoverflow.com/questions/4960208/python-2-7-getting-user-input-and-manipulating-as-string-without-quotations
+                print ("user guess  =", userinput)
+                if userinput == "1" or userinput == "2" or userinput == "3" or userinput == "4" or userinput == "5" or userinput == "6":
+                    answerguess.append(int(userinput))
+                    print (answerguess)
+                else:
+                    print ("\nInvalid input. Please use numbers 1 to 6.")
+                    validinput = False
+        self.answerfinal = [ guessoptions[answerguess[0]], guessoptions[answerguess[1]], guessoptions[answerguess[2]], guessoptions[answerguess[3]] ]
+
+
     def tally(self):
-        ##have to reset the tally first or you will miscount when called
-        ## multiple times.
+        ##have to reset the tally first or you will miscount when called again
         self.answercount = {
             "Red" : 0,
             "Green" : 0,
@@ -117,8 +141,7 @@ class codecreator(object):
                 self.answercount["Yellow"] +=1
 
     def guesstally(self):
-        ##have to reset the tally first or you will miscount when called
-        ## multiple times.
+        ##have to reset the tally first or you will miscount if called again.
         self.guesscount = {
             "Red" : 0,
             "Green" : 0,
@@ -130,9 +153,7 @@ class codecreator(object):
         ### ANSWERFINAL is the completed guess variable ####
         for each in self.answerfinal:
             if each == "Red":
-                #print (self.answercount["Red"])
                 self.guesscount["Red"] +=1
-                #print (self.answercount["Red"], "debug")
             elif each == "Green":
                 self.guesscount["Green"] +=1
             elif each == "Blue":
@@ -152,79 +173,82 @@ class codecreator(object):
         while len(answerguess) < 4:
             validinput = True
             while validinput == True and len(answerguess)<4:
-                userinput =raw_input("Your Guess options are:  1= Red, 2 = Green, 3= Blue, 4 = Black, 5 = White, 6 = Yellow :")
+                userinput =raw_input("Your Guess options are:\n1= Red, 2 = Green, 3= Blue, 4 = Black, 5 = White, 6 = Yellow\nPlease enter a number: ")
                 ## use raw_input instead of input() http://stackoverflow.com/questions/4960208/python-2-7-getting-user-input-and-manipulating-as-string-without-quotations
-                print ("user guess  =", userinput)
+                #print ("user guess  =", userinput)
                 if userinput == "1" or userinput == "2" or userinput == "3" or userinput == "4" or userinput == "5" or userinput == "6":
                     answerguess.append(int(userinput))
                     print (answerguess)
                 else:
                     print ("\nInvalid input. Please use numbers 1 to 6.")
                     validinput = False
+        # Now convert those stored values into the actual guess
         self.answerfinal = [ guessoptions[answerguess[0]], guessoptions[answerguess[1]], guessoptions[answerguess[2]], guessoptions[answerguess[3]] ]
-        
+        print(self.answerfinal)
 
     def wincheck(self):
         self.winner = False
-        colorpeg = []
+        self.colorpeg = []
+        ## Win if it is a match!!!
         if self.answerfinal == self.answer:
-            print ("youwon", self.answerfinal, self.answer)
+            print ("A Winner is YOU! All your BASE! etc...", self.answerfinal, self.answer)
             self.winner = True
             return
-        ### only tally EXACT MATCHES FIRST then decriment  answercount and guesscount accordingly
-        ### the removal of answer count tally and guess count tally is necessary to compute color matches that aren't exact.
-        ##
-        #print ( "Before computation ran the tally of answer is :", self.answercount, "\n     and the tally of guess is ", self.guesscount)
-        ##
-        ### Check for EXACT MATCHES. remove from count tally so they arent double counted as ###
+        
+        ### Count EXACT matches first
+        ### Then Decrement matches out of the tally, so you dont double count them.
         if self.answerfinal[0] == self.answer[0]:
             self.answercount[self.answerfinal[0]] -= 1
             self.guesscount[self.answerfinal[0]] -= 1
-            colorpeg.append("Black")
+            self.colorpeg.append("Black")
         if self.answerfinal[1] == self.answer[1]:
             self.answercount[self.answerfinal[1]] -= 1
             self.guesscount[self.answerfinal[1]] -= 1
-            colorpeg.append("Black")
+            self.colorpeg.append("Black")
         if self.answerfinal[2] == self.answer[2]:
             self.answercount[self.answerfinal[2]] -= 1
             self.guesscount[self.answerfinal[2]] -= 1
-            colorpeg.append("Black")
+            self.colorpeg.append("Black")
         if self.answerfinal[3] == self.answer[3]:
             self.answercount[self.answerfinal[3]] -= 1
             self.guesscount[self.answerfinal[3]] -= 1
-            colorpeg.append("Black")
+            self.colorpeg.append("Black")
 
-        ### Check for color specific matches
-
-            ###Just loop through
+        ### Check for Color Matches next
+        ### do some logic. Count color matches in incorrect spots.    
         colorlooping = ["Red", "Green", "Blue", "Black", "White", "Yellow"]
         for each in colorlooping:
             if self.answercount[each] > 0 and self.answercount[each] == self.guesscount[each]:
                 while self.answercount[each] > 0:
-                    colorpeg.append("White")
+                    self.colorpeg.append("White")
                     self.answercount[each] -= 1
             elif self.answercount[each] > self.guesscount[each]:
                 while self.guesscount[each] > 0:
-                    colorpeg.append("White")
+                    self.colorpeg.append("White")
                     self.guesscount[each] -= 1
             elif self.answercount[each] > 0 and self.answercount[each] < self.guesscount[each]:
                 while self.answercount[each] > 0:
-                    colorpeg.append("White")
+                    self.colorpeg.append("White")
                     self.answercount[each] -= 1            
             
-        ########  I think there is a count bug in here somewhere ##
-        shuffleclues = shuffle(colorpeg)
-        #print (colorpeg, shuffleclues)
+        #Shuffle the match analysis results to make deducting the answer more difficult.
+        shuffleclues = shuffle(self.colorpeg)
         print ("--------------------------------")
-        print ("the matches are ", shuffleclues, )
+        print ("Your Hints are :", shuffleclues, )
 
-### Guessing code ###
+#----------------------------------------------            
+############## SETUP END ######################
+#----------------------------------------------
+
 
               
 #####################
 
 secret_code = codecreator()
 secret_code.random_code()
+previousguesshint = []
+
+
 
 trycount = 10
 while trycount >0 and secret_code.winner == False:
@@ -232,14 +256,12 @@ while trycount >0 and secret_code.winner == False:
     secret_code.tally()
     secret_code.guesstally()
     secret_code.wincheck()
+    previousguesshint.append((secret_code.answerfinal, secret_code.colorpeg))
+    print ("\nYour previous guesses and hints were:")
+    print (previousguesshint,)
     trycount -= 1
     
     
     
-  
-#----------------------------------------------------------------------------------
-            
-################################### SETUP END ###########################################################
 
-#----------------------------------------------------------------------
 
